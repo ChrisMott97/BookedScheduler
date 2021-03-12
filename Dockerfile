a24ef8d32b2af67fb1283bda465f75540fe8c296
Chris <chrisamott97@gmail.com>
2021-03-12 11:57:41 +0000

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM php:7-apache
+WORKDIR /var/www/html/
+RUN mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
+RUN a2enmod rewrite
+RUN a2enmod headers
+RUN docker-php-ext-install mysqli
+RUN docker-php-ext-install pdo_mysql
+COPY . .
+RUN mkdir "$PWD/tpl_c"
+RUN chmod 777 "$PWD/tpl_c" "$PWD/tpl"
+RUN mv "$PWD/config/config.dist.php" "$PWD/config/config.php"
+EXPOSE 80
+
+
--- config/config.dist.php
+++ config/config.dist.php
@@ -19,14 +19,14 @@ along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 /**
  * Application configuration
  */
 $conf['settings']['app.title'] = 'Booked Scheduler';			// application title
-$conf['settings']['default.timezone'] = 'America/New_York';      // look up here http://php.net/manual/en/timezones.php
+$conf['settings']['default.timezone'] = 'Europe/London';      // look up here http://php.net/manual/en/timezones.php
 $conf['settings']['allow.self.registration'] = 'true';         	// if users can register themselves
 $conf['settings']['admin.email'] = 'admin@example.com';         // email address of admin user
 $conf['settings']['admin.email.name'] = 'Booked Administrator';	// name to be used in From: field when sending automatic emails
@@ -112,7 +112,7 @@ $conf['settings']['uploads']['reservation.attachment.extensions'] = 'txt,jpg,gif
 $conf['settings']['database']['type'] = 'mysql';
 $conf['settings']['database']['user'] = 'booked_user';        // database user with permission to the booked database
 $conf['settings']['database']['password'] = 'password';
-$conf['settings']['database']['hostspec'] = '127.0.0.1';        // ip, dns or named pipe
+$conf['settings']['database']['hostspec'] = 'bookedscheduler_db_1';        // ip, dns or named pipe
 $conf['settings']['database']['name'] = 'bookedscheduler';
 /**
  * Mail server configuration
@@ -138,7 +138,7 @@ $conf['settings']['plugins']['PostReservation'] = '';
 /**
  * Installation settings
  */
-$conf['settings']['install.password'] = '';
+$conf['settings']['install.password'] = 'booked';
 /**
  * Pages
  */
