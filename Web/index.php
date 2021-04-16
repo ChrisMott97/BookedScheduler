a89391f74c48fce2c53235ed9ac8d0e565149b3d
Chris M <chrisamott97@gmail.com>
2021-04-16 14:04:38 +0000

    added pod IP header to Web index

--- Web/index.php
+++ Web/index.php
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+header('PodIP: ' . getenv('MY_POD_IP'));
 define('ROOT_DIR', '../');
 
 if (!file_exists(ROOT_DIR . 'config/config.php'))
